03571912f8ea88c18db16034cba13f6567870186
Lamriver123 <ngoclamhttv@gmail.com>
2025-05-14 13:25:14 +0700

    updateapi

--- src/main/java/vn/Second_Hand/marketplace/controller/FeedbackController.java
+++ src/main/java/vn/Second_Hand/marketplace/controller/FeedbackController.java
@@ -3,6 +3,7 @@ package vn.Second_Hand.marketplace.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import vn.Second_Hand.marketplace.dto.responses.ApiResponse;
 import vn.Second_Hand.marketplace.dto.responses.FeedbackResponse;
 import vn.Second_Hand.marketplace.entity.Feedback;
 import vn.Second_Hand.marketplace.service.IFeedbackService;
@@ -17,9 +18,14 @@ public class FeedbackController {
     private final IFeedbackService feedbackService;
 
     @GetMapping("/{productId}")
-    public ResponseEntity<List<FeedbackResponse>> getFeedbacksByProductId(@PathVariable int productId) {
-        List<FeedbackResponse> feedbackResponses = feedbackService.getFeedbackResponsesByProductId(productId);
-        return ResponseEntity.ok(feedbackResponses);
+    public ResponseEntity<ApiResponse<List<FeedbackResponse>>> getFeedbacksByProductId(@PathVariable int productId) {
+        List<FeedbackResponse> feedbacks = feedbackService.getFeedbackResponsesByProductId(productId);
+        ApiResponse<List<FeedbackResponse>> response = ApiResponse.<List<FeedbackResponse>>builder()
+                .message("Get feedback successfully")
+                .data(feedbacks)
+                .build();
+        return ResponseEntity.ok(response);
     }
+
 
 }
